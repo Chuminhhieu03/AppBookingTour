11854e3ba451ce2884ad01f82cb6980aa52c0057
Chuminhhieu03 <chuhieuqb2003@gmail.com>
2025-10-26 16:39:50 +0700

    thêm chức năng đổi mật khẩu, và sửa lại middleware cho chính xác hơn

--- AppBookingTour.Application/Features/Auth/Login/LoginCommandValidator.cs
+++ AppBookingTour.Application/Features/Auth/Login/LoginCommandValidator.cs
@@ -11,10 +11,10 @@ public class LoginCommandValidator : AbstractValidator<LoginCommand>
 
         RuleFor(x => x.Email)
             .NotEmpty().WithMessage("Cần phải nhập email để tiếp tục")
-            .EmailAddress().WithMessage("Invalid email format");
+            .EmailAddress().WithMessage("Email không đúng định dạng");
 
         RuleFor(x => x.Password)
-            .NotEmpty().WithMessage("Password is required")
-            .MinimumLength(6).WithMessage("Password must be at least 6 characters");
+            .NotEmpty().WithMessage("Cần phải nhập mật khẩu để đăng nhậ")
+            .MinimumLength(6).WithMessage("Mật khẩu phải có độ dài là 6 ký tự");
     }
 }
