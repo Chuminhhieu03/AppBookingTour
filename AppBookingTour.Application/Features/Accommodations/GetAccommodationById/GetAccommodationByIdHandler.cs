6c0b93f9ed50ec3eef199b3d23391a22426ae0f9
hiepnt <tienhiep345@gmail.com>
2025-11-30 01:54:22 +0700

    Thêm trang chi tiết cơ sở lưu trú ơ khách hàng

--- AppBookingTour.Application/Features/Accommodations/GetAccommodationById/GetAccommodationByIdHandler.cs
+++ AppBookingTour.Application/Features/Accommodations/GetAccommodationById/GetAccommodationByIdHandler.cs
@@ -16,29 +16,58 @@ namespace AppBookingTour.Application.Features.Accommodations.GetAccommodationByI
 
         public async Task<GetAccommodationByIdDTO> Handle(GetAccommodationByIdQuery request, CancellationToken cancellationToken)
         {
+            // Lấy accommodation
             var accommodation = await _unitOfWork.Accommodations.GetById(request.id);
+
             if (accommodation == null)
-            {
                 throw new Exception(Message.NotFound);
-            }
-            accommodation.AmenityName = GetListAmenityName(accommodation);
-            var listInfoImg = await _unitOfWork.Images.GetListImageByEntityIdAndEntityType(request.id, Domain.Enums.EntityType.Accommodation);
-            accommodation.ListInfoImage = listInfoImg;
+
+            // Amenity
+            accommodation.AmenityName = GetListAmenityName(accommodation.Amenities);
+
+            // Ảnh của accommodation
+            accommodation.ListInfoImage = await _unitOfWork.Images
+                .GetListImageByEntityIdAndEntityType(request.id, Domain.Enums.EntityType.Accommodation);
+
+            // Status name
             accommodation.StatusName = Constants.ActiveStatus.dctName[Convert.ToInt32(accommodation.IsActive)];
+
+            // Type name
             if (accommodation.Type.HasValue)
                 accommodation.TypeName = Constants.AccommodationType.dctName[accommodation.Type.Value];
-            var listRoomType = accommodation.ListRoomType?.OrderBy(x => -x.Id).ToList();
+
+            // ROOM TYPES
+            var listRoomType = accommodation.ListRoomType?
+                .OrderByDescending(x => x.Id)
+                .ToList();
+
+            if (listRoomType != null)
+            {
+                foreach (var item in listRoomType)
+                {
+                    // Amenity name của room type
+                    item.AmenityName = GetListAmenityName(item.Amenities);
+
+                    // Lấy ảnh room type
+                    var roomTypeImages = await _unitOfWork.Images
+                        .GetListImageByEntityIdAndEntityType(item.Id, Domain.Enums.EntityType.RoomType);
+
+                    item.ListInfoImage = roomTypeImages;
+                }
+            }
+
             accommodation.ListRoomType = listRoomType;
+
             return new GetAccommodationByIdDTO
             {
                 Accommodation = accommodation,
                 Success = true
             };
         }
-
-        private string GetListAmenityName(Accommodation accommodation)
+        
+        private string GetListAmenityName(string amenities)
         {
-            var listAmenityIDStr = accommodation.Amenities?.Split(", ").ToList() ?? new List<string>();
+            var listAmenityIDStr = amenities?.Split(", ").ToList() ?? new List<string>();
 
             var listAmenityID = listAmenityIDStr
                 .Select(x => int.Parse(x))
